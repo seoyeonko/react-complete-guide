aeb8d3968322c75c5b6b5f2edf812968ba1bdb75
seoyeonko <seoyeon99o@naver.com>
2023-03-08 00:57:17 +0900

    section5: rendering list

--- src/components/NewExpense/ExpensesFilter.js
+++ src/components/NewExpense/ExpensesFilter.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
+  console.log('d', props.selected);
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
   };
@@ -11,7 +12,7 @@ const ExpensesFilter = (props) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select selected={props.selected} onChange={dropdownChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
