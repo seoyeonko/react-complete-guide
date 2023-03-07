ff46fb47c8e3b345a7e47981030e01823da9686e
seoyeonko <seoyeon99o@naver.com>
2023-03-08 00:21:16 +0900

    section4: react state & event

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (extendExpenseData) => {
+    const expenseData = {
+      ...extendExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
